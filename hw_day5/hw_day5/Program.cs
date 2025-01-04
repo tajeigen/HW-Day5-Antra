3fb5bcf83ac1f4161676a38d9d2fee9302aa657d
tajeigen <tajerramt@proton.me>
2025-01-03 22:23:01 -0600

    forgot

---
+++ hw_day5/hw_day5/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace hw_day5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Welcome to Hacker Name!");
+        
+        Console.WriteLine("What s your faborite Color? ");
+        string color = Console.ReadLine();
+        Console.WriteLine($"Your name is : {color}");
+        
+        Console.WriteLine("What s your astrological sign? ");
+        string sign = Console.ReadLine();
+        Console.WriteLine($"Your sign is : {sign}");
+        
+        Console.WriteLine("What's your street address number? ");
+        int nums =  Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"Your number is : {nums}");
+        
+        //hacker name is: 
+        string hackerName = color + sign + Convert.ToString(nums);
+        Console.WriteLine($"Your hacker name is : {hackerName}");
+    }
+}
